50808fcff2701052a560227836da512b7995aa87
Nicholas Alder <nickoluss@macaronndcheeze.uguest.utah.edu>
2018-06-04 20:48:02 -0600

    java file

--- javascript.js
+++ javascript.js
@@ -1,8 +1,22 @@
-<html>  
+
 <head>  
 <script type="text/javascript" src="message.js"></script>  
 </head>  
 <body>   
- <script></script>
+ <script>
+
+<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBgFsP0ttl-Cm_uFIFBjPuBZQfxW7O7uOA",
+    authDomain: "hikingproject-33.firebaseapp.com",
+    databaseURL: "https://hikingproject-33.firebaseio.com",
+    projectId: "hikingproject-33",
+    storageBucket: "hikingproject-33.appspot.com",
+    messagingSenderId: "970307116122"
+  };
+  firebase.initializeApp(config);
+</script>
+ </script>
 </body>  
-</html>  
