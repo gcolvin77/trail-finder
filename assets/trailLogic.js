ffff9100da09a6a9d4c2a09a082d6e9b584194e9
Ryan Simpson <ryan@jacobsandsimpson.com>
2018-06-06 20:52:46 -0600

    add js and update index

--- assets/trailLogic.js
+++ assets/trailLogic.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-   
+
 });
 
 var longtitude;
@@ -8,17 +8,15 @@ var inputRadius;
 var inputResults;
 
 
-function zipCode(){
+function zipCode() {
     event.preventDefault();
     var inputZip = $(".zip-input").val().trim();
-    
+
     // console.log(inputZip);
     // console.log(inputRadius);
 
-    var queryURL="https://maps.googleapis.com/maps/api/geocode/json?address="+inputZip+"&key=AIzaSyARlMLF1Ol1VfJcOZNDY2XsA0bD4YjCbps"
-    //var queryURL="https://maps.googleapis.com/maps/api/geocode/json?address=salt+lake+city,+ut&key=AIzaSyARlMLF1Ol1VfJcOZNDY2XsA0bD4YjCbps"
+    var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + inputZip + "&key=" + APIKeys.google
 
- 
 
 
     // Creating an AJAX call for the specific movie button being clicked
@@ -27,11 +25,11 @@ function zipCode(){
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        latitude=response.results[0].geometry.location.lat;
-        longitude=response.results[0].geometry.location.lng;
-        console.log("lattitude: "+ response.results[0].geometry.location.lat);
-        console.log("longitude: "+ response.results[0].geometry.location.lng);
-        console.log("location: "+ response.results[0].formatted_address);
+        latitude = response.results[0].geometry.location.lat;
+        longitude = response.results[0].geometry.location.lng;
+        console.log("lattitude: " + response.results[0].geometry.location.lat);
+        console.log("longitude: " + response.results[0].geometry.location.lng);
+        console.log("location: " + response.results[0].formatted_address);
         trailTest();
     })
 };
@@ -40,18 +38,15 @@ function trailTest() {
     //clear div
     //event.preventDefault();
     inputRadius = $(".radius-input").val().trim();
-    if (inputRadius==="") {
-       inpurRadius=10;
+    if (inputRadius === "") {
+        inpurRadius = 10;
     }
     inputResults = $(".results-input").val().trim();
-    if (inputResults==="") {
-        inputResults=10;
+    if (inputResults === "") {
+        inputResults = 10;
     }
 
-    //var animal = $(this).attr("data-name");
-    //var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=7noUkTFzs8VToSoelsrj21GOFUlx5ulX&limit=10";
-    //var queryURL="https://www.hikingproject.com/data/get-trails?lat="+latitude+"&lon="+longitude+"&maxDistance=10&maxResults=20&key=200284172-9235280cfd41cc587bea45bb35200992"
-    var queryURL="https://www.hikingproject.com/data/get-trails?lat="+latitude+"&lon="+longitude+"&maxDistance="+inputRadius+"&maxResults="+inputResults+"&key=200284172-9235280cfd41cc587bea45bb35200992"
+    var queryURL = "https://www.hikingproject.com/data/get-trails?lat=" + latitude + "&lon=" + longitude + "&maxDistance=" + inputRadius + "&maxResults=" + inputResults + "&key=" + APIKeys.hiking
 
     // Creating an AJAX call for the specific movie button being clicked
     $.ajax({
@@ -62,20 +57,20 @@ function trailTest() {
         console.log(response.trails.length);
         console.log("longitude: " + longitude);
         console.log("latitude: " + latitude);
-        
-        var trailName="";
-        var trailLocation="";
-        var trailAscent="";
-        var trailStars="";
-        var trailDifficulty="";
+
+        var trailName = "";
+        var trailLocation = "";
+        var trailAscent = "";
+        var trailStars = "";
+        var trailDifficulty = "";
         $(".trail-results").empty();
-        for (i=0; i<response.trails.length; i++) {
-            trailName=response.trails[i].name;
-            trailLocation=response.trails[i].location;
-            trailAscent=response.trails[i].length;
-            trailStars=response.trails[i].stars;
-            trailDifficulty=response.trails[i].difficulty;
-            $(".trail-results").append("<tr><td>" + trailName+ "</td><td>" + trailLocation+ "</td><td>" + trailAscent+ "</td><td>" +trailAscent+ "</td><td>" +trailStars+ "</td><td>" +trailDifficulty+ "</td><td>" +"</td></tr>");
+        for (i = 0; i < response.trails.length; i++) {
+            trailName = response.trails[i].name;
+            trailLocation = response.trails[i].location;
+            trailAscent = response.trails[i].length;
+            trailStars = response.trails[i].stars;
+            trailDifficulty = response.trails[i].difficulty;
+            $(".trail-results").append("<tr><td>" + trailName + "</td><td>" + trailLocation + "</td><td>" + trailAscent + "</td><td>" + trailAscent + "</td><td>" + trailStars + "</td><td>" + trailDifficulty + "</td><td>" + "</td></tr>");
         };
 
     })
