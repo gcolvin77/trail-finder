d982e7df5d951b15fb469b2f4352735e3ad3963d
Ryan Simpson <ryan@jacobsandsimpson.com>
2018-06-09 09:19:03 -0600

    updated logic for search and added class to index form fields

--- assets/trailLogic.js
+++ assets/trailLogic.js
@@ -10,28 +10,73 @@ var inputResults;
 
 function zipCode() {
     event.preventDefault();
+    var inputCity = $(".city-input").val().trim();
+    var outputCity = "";
+    if (inputCity != "") {
+        outputCity = inputCity + ", ";
+    };
+    var inputState = $(".state-input").val().trim();
+    var outputState = "";
+    if (inputState != "") {
+        outputState = inputState + " ";
+    };
     var inputZip = $(".zip-input").val().trim();
+    var outputZip = "";
+    if (inputZip != "") {
+        outputZip = inputZip;
+    };
+    var inputCombined = "";
+    var outputCombined = "";
+    var finalOutput = "";
 
-    // console.log(inputZip);
-    // console.log(inputRadius);
+    //logic for input
+    //if location is empty then display an alert
+    if (inputCity === "" && inputState === "" && inputZip === "") {
+        alert("Please enter a location");
+        //runs code if location is not empty
+    } else {
+        inputCombined = (outputCity + outputState + outputZip).trim();
+        outputCombined = inputCombined.split(" ");
+        for (i = 0; i < outputCombined.length; i++) {
+            console.log(outputCombined[i]);
+            finalOutput = finalOutput + outputCombined[i];
+            if (i < outputCombined.length - 1) {
+                finalOutput = finalOutput + "+";
+            }
+        };
 
-    var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + inputZip + "&key=" + APIKeys.google
+
+        console.log("input: " + inputCombined);
+        console.log("output: " + outputCombined);
+        console.log("final output: " + finalOutput);
+
+
+        // console.log(inputZip);
+        // console.log(inputRadius);
+
+        var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + finalOutput + "&key=" + APIKeys.google
 
 
 
-    // Creating an AJAX call for the specific movie button being clicked
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        latitude = response.results[0].geometry.location.lat;
-        longitude = response.results[0].geometry.location.lng;
-        console.log("lattitude: " + response.results[0].geometry.location.lat);
-        console.log("longitude: " + response.results[0].geometry.location.lng);
-        console.log("location: " + response.results[0].formatted_address);
-        trailTest();
-    })
+        // Creating an AJAX call
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            // if location returns more than 1 location, then does not run call
+            if (response.results.length > 1) {
+                alert("Please enter a valid location");
+            } else {
+                latitude = response.results[0].geometry.location.lat;
+                longitude = response.results[0].geometry.location.lng;
+                console.log("lattitude: " + response.results[0].geometry.location.lat);
+                console.log("longitude: " + response.results[0].geometry.location.lng);
+                console.log("location: " + response.results[0].formatted_address);
+                trailTest();
+            }
+        })
+    };
 };
 
 function trailTest() {
@@ -39,11 +84,13 @@ function trailTest() {
     //event.preventDefault();
     inputRadius = $(".radius-input").val().trim();
     if (inputRadius === "") {
-        inpurRadius = 10;
+        inpurRadius = 30;
     }
     inputResults = $(".results-input").val().trim();
     if (inputResults === "") {
         inputResults = 10;
+    } else if (inputResults > 500) {
+        inputResults = 500;
     }
 
     var queryURL = "https://www.hikingproject.com/data/get-trails?lat=" + latitude + "&lon=" + longitude + "&maxDistance=" + inputRadius + "&maxResults=" + inputResults + "&key=" + APIKeys.hiking
@@ -61,22 +108,48 @@ function trailTest() {
         var trailName = "";
         var trailLocation = "";
         var trailAscent = "";
+        var trailLength = "";
         var trailStars = "";
         var trailDifficulty = "";
+        var trailID = "";
         $(".trail-results").empty();
         for (i = 0; i < response.trails.length; i++) {
             trailName = response.trails[i].name;
             trailLocation = response.trails[i].location;
-            trailAscent = response.trails[i].length;
+            trailLength = response.trails[i].length;
+            trailAscent = response.trails[i].ascent;
             trailStars = response.trails[i].stars;
             trailDifficulty = response.trails[i].difficulty;
-            $(".trail-results").append("<tr><td>" + trailName + "</td><td>" + trailLocation + "</td><td>" + trailAscent + "</td><td>" + trailAscent + "</td><td>" + trailStars + "</td><td>" + trailDifficulty + "</td><td>" + "</td></tr>");
+            trailID = response.trails[i].id
+
+            $(".trail-results").append("<tr><td>" + trailName + "</td><td>" + trailLocation + "</td><td>" + trailLength + "</td><td>" + trailAscent + "</td><td>" + trailStars + "</td><td>" + trailDifficulty + "</td><td>" + "<button class= new-trail-id ID=" + trailID + ">View Trail</button>" + "</td></tr>");
         };
 
     })
 };
 
-//display giphy for existing buttons
+//function to open new window for specific trail
+function newTrail() {
+    var currBtn = $(this).attr('ID');
+    //alert(currBtn);
+    var queryURL = "https://www.hikingproject.com/data/get-trails-by-id?ids=" + currBtn + "&key=" + APIKeys.hiking;
+
+    // Creating an AJAX call for the specific trail button being clicked
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        trailURL=response.trails[0].url;
+        console.log(trailURL);
+    });
+};
+
+
+//call search function on click of submit button
 $(document).on("click", ".search", zipCode);
+
+//call search function on click of submit button
+$(document).on("click", ".new-trail-id", newTrail);
 
 
